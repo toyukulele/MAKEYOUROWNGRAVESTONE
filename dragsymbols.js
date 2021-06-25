d3ec674dc6d66d3d0b8a63e15cfbb72c6d136d65
toyukulele <toyukulele@gmail.com>
2021-06-25 17:46:42 +0300

    в

--- dragsymbols.js
+++ dragsymbols.js
@@ -26,15 +26,11 @@ Array.from(symbols)
       document.body.append(symbol);
       symbol.style.zIndex = 1000;
 
-      moveAt(event.pageX, event.pageY);
-
       function moveAt(pageX, pageY) {
-        symbol.style.left = (pageX - shiftX) / 10 + "rem";
-        symbol.style.top = (pageY - shiftY) / 10 + "rem";
-      }
-
-      function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
+        symbol.style.left =
+          (pageX - shiftX) / ((10 * window.innerWidth) / 1440) + "rem";
+        symbol.style.top =
+          (pageY - shiftY) / ((10 * window.innerWidth) / 1440) + "rem";
       }
 
       document.addEventListener("mousemove", onMouseMove);
